f420ec6d5d897d7457d64215d17f54999abf98fe
calvinhobbeson-java <calvinhobbeson.java@gmail.com>
2025-05-11 16:13:49 +0300

    4. TODO list [#3786] поправил view

--- src/main/java/ru/job4j/todo/controller/TaskController.java
+++ src/main/java/ru/job4j/todo/controller/TaskController.java
@@ -1,5 +1,6 @@
 package ru.job4j.todo.controller;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -45,5 +46,58 @@ public class TaskController {
             model.addAttribute("message", e.getMessage());
             return "errors/404";
         }
+    }
+
+    @GetMapping("/update")
+    public String getUpdatePage(Model model) {
+        return "task/update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Task task, Model model) {
+        try {
+            var isUpdated = service.update(task);
+            if (!isUpdated) {
+                model.addAttribute("message", "Update was not completed");
+                return "errors/404";
+            }
+            return "redirect:/index";
+        } catch (Exception e) {
+            model.addAttribute("message", e.getMessage());
+            return "errors/404";
+        }
+    }
+
+    @PostMapping("/delete")
+    public String deleteById(Model model, @PathVariable int id) {
+        try {
+            var isDeleted = service.deleteById(id);
+            if (!isDeleted) {
+                model.addAttribute("message", "Update was not completed");
+                return "errors/404";
+            }
+            return "redirect:/index";
+        } catch (Exception e) {
+            model.addAttribute("message", e.getMessage());
+            return "errors/404";
+        }
+    }
+
+    @GetMapping("/done")
+    public String getDone(Model model) {
+        model.addAttribute("tasks", service.findDone());
+        return "tasks/index";
+    }
+
+    @GetMapping("/new")
+    public String getNew(Model model) {
+        model.addAttribute("tasks", service.findNew());
+        return "tasks/index";
+    }
+    @GetMapping("/{taskId}")
+    public String getSingleTask(@PathVariable int taskId, Model model) {
+        var taskOptional = service.findById(taskId);
+        model.addAttribute("task", taskOptional.get());
+        return "tasks/one";
     }
 }
